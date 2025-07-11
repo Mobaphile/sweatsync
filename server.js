f76ca999d14edcb1a14189756f786474c00e1fdf
Mobaphile <josh.martignoni@gmail.com>
2025-07-11 01:20:29 +0000

    running server, no funct, no understanding.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const authRoutes = require('./routes/auth');
-const workoutRoutes = require('./routes/workouts');
+const authRoutes = require('./server/routes/auth');
+const workoutRoutes = require('./server/routes/workouts');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
