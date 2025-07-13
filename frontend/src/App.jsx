2118c028ec804f6a385d4bdd4c7772ac0f3b8e2d
Mobaphile <josh.martignoni@gmail.com>
2025-07-12 23:39:05 -0400

    Now History and Plan view work....

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -813,7 +813,7 @@ const App = () => {
           </div>
         )}
 
-         {/* HISTORY VIEW */}
+        {/* HISTORY VIEW */}
         {currentView === 'history' && (
           <div>
             <h2 className="text-3xl font-bold text-gray-900 mb-8">Workout History</h2>
